ee1d9a7dd79af62faa604d08bf4289f12c1016de
jy3736 <jyang@stust.edu.tw>
2022-01-13 09:34:22 +0800

    test

---
+++ week01/lab01/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n");
+    printf("STUST : Hello C/C++ World");
+    printf("\n\n");
+    return 0;
+}
---
+++ week01/lab02/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n"
+         << "STUST : Hello C++ World!"
+         << "\n\n";
+
+    return 0;
+}

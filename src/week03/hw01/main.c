aff3a093db135c8bcffd85295183c103db5e6dc7
Young Yang <jyang@stust.edu.tw>
2022-03-01 13:07:02 +0800

    jyang

---
+++ src/week03/hw01/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // write your code here
+    printf("=========================\n\n");
+    printf("姓名  : %s\n","Young Yang");
+    printf("學號  : %s\n","CS1681688");
+    printf("電郵  : %s\n","jy3736@eecs.utah.edu");
+    printf("科系  : %s\n","電子工程系");
+    printf("=========================\n\n");
+
+    return 0;
+} 

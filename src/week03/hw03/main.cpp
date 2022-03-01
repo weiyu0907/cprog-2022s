9406b0bc702d4bb1e3eacec1c25c71ef837c4ca9
Young Yang <jyang@stust.edu.tw>
2022-03-01 13:55:46 +0800

    jyang

---
+++ src/week03/hw03/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    int c;
+
+    cout<<"請輸入一個整數 (n)  ：";
+    cin>>n;
+    cout<<"請輸入一個整數 (列) ：";
+    cin>>c;
+    
+    // write your code here
+
+    return 0;
+}

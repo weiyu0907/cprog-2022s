95c471c894136dfe0d7f1e3a7e3c7903a0e8765e
jy3736 <jyang@stust.edu.tw>
2022-03-17 09:46:31 +0800

    jyang

--- docs/dark-mode.py
+++ docs/dark-mode.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! /usr/bin/python
 
 import sys, os
@@ -12,7 +11,6 @@ add_to_head="""
 add_to_body="""
 <script>hljs.highlightAll();</script>
 """
-
 
 def main(name):
     ofn = name.replace(".html","-dark.html")
@@ -33,29 +31,3 @@ if __name__=="__main__":
         main(sys.argv[1])
     else:
         print("\nUsage : dark-mode.py <html-file-name>\n")
-=======
-#! /usr/bin/python
-
-import sys, os
-
-
-def main(name):
-    ofn = name.replace(".html","-dark.html")
-    fh = open(name,encoding="utf-8") 
-    text = ""
-    for l in fh:
-        text+=l
-    text = text.replace("</head",' <link rel="stylesheet" href="dark-theme.css">  </head')
-    text = text.replace('href="https:',' target="_blank" href="https:')    
-    dark = open(ofn, 'w', encoding='UTF-8')
-    dark.write(text)
-    dark.close()
-    fh.close()
-    
-if __name__=="__main__":
-    if len(sys.argv)==2:
-        main(sys.argv[1])
-    else:
-        print("\nUsage : dark-mode.py <html-file-name>\n")
->>>>>>> b4ea6034f2f020aacf69145e6917d8c6092888c0
-    
